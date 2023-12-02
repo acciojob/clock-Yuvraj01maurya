372114fa00321f67d80613e1280ecdf6da041144
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-02 20:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Timer
+const timer = document.getElementById("timer");
+setInterval(() => {
+    let instance = new Date();
+    // date = mm/dd/yyyy
+    const date = `${instance.getMonth() + 1}/${instance.getDate()}/${instance.getFullYear()}`;
+
+    let hrs = instance.getHours(); // 24hr clock
+    const ampm = hrs >= 12 ? "PM" : "AM";
+
+    // 22:31 => 10:31 PM
+    hrs > 12 && (hrs -= 12);
+
+    // time = hh:mm:ss PM
+    const time = `${hrs}:${instance.getMinutes()}:${instance.getSeconds()} ${ampm}`
+    timer.innerText = `${date} , ${time}`
+}, 1000)
